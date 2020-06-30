fddc5fd21608afaef9289362cc3e2afc76301c1b
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-06-30 12:36:05 -0700

    add ability to list shows

--- api_demo.gemspec
+++ api_demo.gemspec
@@ -31,4 +31,5 @@ Gem::Specification.new do |spec|
   spec.add_dependency "httparty"
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", ">= 12.3.3"
+  spec.add_development_dependency "pry"
 end
--- lib/api_demo/cli.rb
+++ lib/api_demo/cli.rb
@@ -2,9 +2,13 @@ module ApiDemo
   class CLI 
     def start 
       puts "Welcome to our demo API"
+      list_shows
+    end
+
+    def list_shows
       Tvshow.load
-      Tvshow.all.each do |show|
-        puts show.name
+      Tvshow.all.each.with_index(1) do |show, index|
+        puts "#{index}. #{show.name}"
       end
     end
   end
